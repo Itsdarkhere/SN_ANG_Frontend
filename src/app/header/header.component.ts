12435c94426516acd3f79dbabba11243aab28a7c
raj.dixit_HTML <raj.dixit@appinventiv.com>
2021-08-21 22:05:32 +0530

    update the header and work on the links of profile, notification, chat & done some sort of UI imporvements

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { AppRoutingModule } from '../app-routing.module';
+import { GlobalVarsService } from '../global-vars.service';
 
 @Component({
   selector: 'app-header',
@@ -7,9 +9,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeaderComponent implements OnInit {
 
-  constructor() { }
+  AppRoutingModule = AppRoutingModule;
+
+  constructor(public globalVars: GlobalVarsService) {}
 
   ngOnInit(): void {
   }
+
 
 }

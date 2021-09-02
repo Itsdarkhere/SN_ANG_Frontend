25ad2a94f015b40966c8026acb1d36e6567fa40d
Andrzej <andrzej@yexperiment.com>
2021-09-03 01:05:44 +0200

    Profile descriptions up to 512 characters

--- src/app/update-profile-page/update-profile/update-profile.component.ts
+++ src/app/update-profile-page/update-profile/update-profile.component.ts
@@ -133,7 +133,7 @@ export class UpdateProfileComponent implements OnInit, OnChanges {
       this.profileUpdateErrors.usernameError = false;
     }
 
-    if (this.descriptionInput.length > 256) {
+    if (this.descriptionInput.length > 512) {
       this.profileUpdateErrors.descriptionError = true;
       hasErrors = true;
     } else {

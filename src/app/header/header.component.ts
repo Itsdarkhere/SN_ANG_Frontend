fa1e7f50581d9f6095feda926092e47a04905764
Valtteri Juvonen <valtteri.juvone@hotmail.com>
2021-09-18 18:32:48 +0300

    mobile messages fix

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,67 +1,64 @@
-import { Component, OnInit, Input } from '@angular/core';
-import { AppRoutingModule } from '../app-routing.module';
-import { CreatePostComponent } from '../create-post/create-post.component';
-import { CreatePostUploadMintComponent } from '../create-post-upload-mint/create-post-upload-mint.component';
-import { GlobalVarsService } from '../global-vars.service';
-import { MatDialog } from '@angular/material/dialog';
-import { MintYourNftComponent } from '../mint-your-nft/mint-your-nft.component';
-import { CreateYourNftComponent } from '../create-your-nft/create-your-nft.component';
-import { PlaceABidComponent } from '../place-a-bid/place-a-bid.component';
+import { Component, OnInit, Input } from "@angular/core";
+import { AppRoutingModule } from "../app-routing.module";
+import { CreatePostComponent } from "../create-post/create-post.component";
+import { CreatePostUploadMintComponent } from "../create-post-upload-mint/create-post-upload-mint.component";
+import { GlobalVarsService } from "../global-vars.service";
+import { MatDialog } from "@angular/material/dialog";
+import { MintYourNftComponent } from "../mint-your-nft/mint-your-nft.component";
+import { CreateYourNftComponent } from "../create-your-nft/create-your-nft.component";
+import { PlaceABidComponent } from "../place-a-bid/place-a-bid.component";
 
 @Component({
-  selector: 'app-header',
-  templateUrl: './header.component.html',
-  styleUrls: ['./header.component.scss']
+  selector: "app-header",
+  templateUrl: "./header.component.html",
+  styleUrls: ["./header.component.scss"],
 })
 export class HeaderComponent implements OnInit {
-
   @Input() imgSrc: string;
-
 
   isNotificationOpen: boolean = false;
   isSearchOpen: boolean = false;
 
   AppRoutingModule = AppRoutingModule;
 
-  constructor(public globalVars: GlobalVarsService, public dialog: MatDialog) { }
+  constructor(public globalVars: GlobalVarsService, public dialog: MatDialog) {}
 
   createPostUploadMint(): void {
     const dialogRef = this.dialog.open(CreatePostUploadMintComponent, {
-      width: '620px',
-      panelClass: 'popup-modal'
+      width: "620px",
+      panelClass: "popup-modal",
     });
   }
 
   createPost(): void {
     const dialogRef = this.dialog.open(CreatePostComponent, {
-      width: '480px',
-      panelClass: 'popup-modal'
+      width: "480px",
+      panelClass: "popup-modal",
     });
   }
 
   mintYourNFT(): void {
     const dialogRef = this.dialog.open(MintYourNftComponent, {
-      width: '620px',
-      panelClass: 'popup-modal'
+      width: "620px",
+      panelClass: "popup-modal",
     });
   }
 
   createYourNFT(): void {
     const dialogRef = this.dialog.open(CreateYourNftComponent, {
-      width: '620px',
-      panelClass: 'popup-modal'
+      width: "620px",
+      panelClass: "popup-modal",
     });
   }
 
   placeBid(): void {
     const dialogRef = this.dialog.open(PlaceABidComponent, {
-      width: '600px',
-      panelClass: 'popup-modal'
+      width: "600px",
+      panelClass: "popup-modal",
     });
   }
 
-  ngOnInit(): void {
-  }
+  ngOnInit(): void {}
 
   showNotification() {
     this.isNotificationOpen = !this.isNotificationOpen;
@@ -74,9 +71,6 @@ export class HeaderComponent implements OnInit {
     //console.log(document.getElementById('mb-inp-searchbar'));
   }
 
-
-  
-
   homeLink(): string {
     if (this.globalVars.showLandingPage()) {
       return "/" + this.globalVars.RouteNames.LANDING;
@@ -84,10 +78,10 @@ export class HeaderComponent implements OnInit {
       return "/" + this.globalVars.RouteNames.BROWSE;
     }
   }
-  closeDropdown(){
+  closeDropdown() {
     this.isNotificationOpen = false;
   }
-  clickOutside(){
+  clickOutside() {
     this.isNotificationOpen = false;
   }
 }

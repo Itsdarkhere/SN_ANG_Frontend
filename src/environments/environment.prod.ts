ee60d4aec8ab8250679328850ebafea5153b370e
Andrzej <andrzej@yexperiment.com>
2021-10-01 01:32:27 +0200

    Make bitclout the backend for video

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,7 +2,7 @@ export const environment = {
   production: true,
   uploadImageHostname: "bitclout.com",
   jumioEndpointHostname: "bitclout.com",
-  uploadVideoHostname: "love4src.com",
+  uploadVideoHostname: "bitclout.com",
   dd: {
     apiKey: "DCEB26AC8BF47F1D7B4D87440EDCA6",
     jsPath: "https://bitclout.com/tags.js",

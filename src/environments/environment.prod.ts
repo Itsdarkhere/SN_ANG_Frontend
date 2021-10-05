e931b26aab739c1fa4b204eafcd6f87784ebbf43
Andrzej <andrzej@yexperiment.com>
2021-10-05 14:09:59 +0200

    Revert upload of videos back to bitclout.com

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,7 +2,7 @@ export const environment = {
   production: true,
   uploadImageHostname: "bitclout.com",
   jumioEndpointHostname: "bitclout.com",
-  uploadVideoHostname: "tijn.club",
+  uploadVideoHostname: "bitclout.com",
   identityURL: "https://identity.deso.org",
   dd: {
     apiKey: "DCEB26AC8BF47F1D7B4D87440EDCA6",

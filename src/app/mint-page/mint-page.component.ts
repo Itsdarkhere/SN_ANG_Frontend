b27c5f62e97886579bba3ae6c6885959d0e6d297
Valtteri Juvonen <valtteri.juvone@hotmail.com>
2021-11-11 23:08:02 +0100

    works mostly

--- src/app/mint-page/mint-page.component.ts
+++ src/app/mint-page/mint-page.component.ts
@@ -41,7 +41,7 @@ import { CommentModalComponent } from "../comment-modal/comment-modal.component"
 export class MintPageComponent implements OnInit {
   @Output() postCreated = new EventEmitter();
 
-  step = 3;
+  step = 1;
   mobile = false;
   submittingPost = false;
   postInput = "";

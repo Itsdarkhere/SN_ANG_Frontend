4ee0b5e41f8fdbf7587164c6c5ea7666ae753e90
Valtteri Juvonen <valtteri.juvone@hotmail.com>
2021-12-07 12:38:53 +0100

    add script src targets, remove unsafe eval

--- src/app/bundlr-client/transaction.ts
+++ src/app/bundlr-client/transaction.ts
@@ -1,22 +1,21 @@
 import { createData, DataItem, DataItemCreateOptions } from "arbundles";
-import { Signer } from "arbundles/src/signing/Signer"
+import { Signer } from "arbundles/src/signing/Signer";
 import Bundlr from "./bundlr";
 
 export default class BundlrTransaction extends DataItem {
-    private bundlr: Bundlr;
-    private signer: Signer;
+  private bundlr: Bundlr;
+  private signer: Signer;
 
-    constructor(data: string | Uint8Array, bundlr: Bundlr, opts?: DataItemCreateOptions) {
-        super(createData(data, bundlr.currencyConfig.getSigner(), opts).getRaw())
-        this.bundlr = bundlr;
-        this.signer = bundlr.currencyConfig.getSigner();
-    }
+  constructor(data: string | Uint8Array, bundlr: Bundlr, opts?: DataItemCreateOptions) {
+    super(createData(data, bundlr.currencyConfig.getSigner(), opts).getRaw());
+    this.bundlr = bundlr;
+    this.signer = bundlr.currencyConfig.getSigner();
+  }
 
-    public sign(): Promise<Buffer> {
-        return super.sign(this.signer);
-    }
-    async upload(): Promise<any> {
-        return this.bundlr.uploader.dataItemUploader(this);
-    }
-
+  public sign(): Promise<Buffer> {
+    return super.sign(this.signer);
+  }
+  async upload(): Promise<any> {
+    return this.bundlr.uploader.dataItemUploader(this);
+  }
 }

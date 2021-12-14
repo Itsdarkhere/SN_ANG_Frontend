24df8690a87dfd3a4c0a2648714401fecc68e0b8
Valtteri Juvonen <valtteri.juvone@hotmail.com>
2021-12-14 17:40:41 +0100

    audio player works

--- src/app/audio-player/audio-player.component.ts
+++ src/app/audio-player/audio-player.component.ts
@@ -1,5 +1,6 @@
-import { Component, Input, OnInit } from "@angular/core";
+import { Component, Input, OnChanges, OnInit } from "@angular/core";
 import Amplitude from "amplitudejs/dist/amplitude.js";
+import { parse } from "path/posix";
 
 @Component({
   selector: "app-audio-player",
@@ -7,20 +8,29 @@ import Amplitude from "amplitudejs/dist/amplitude.js";
   styleUrls: ["./audio-player.component.scss"],
 })
 export class AudioPlayerComponent implements OnInit {
-  @Input() songName: string;
-  @Input() creator: string;
-  @Input() audioSrc: string;
   constructor() {}
+  @Input() songName: any;
+  @Input() creator: any;
+  @Input() audioSrc: any;
 
   ngOnInit(): void {
+    console.log(this.songName + " " + this.creator + " " + this.audioSrc);
+    // Sets volume color correctly on init
+    this.volumeColor();
     Amplitude.init({
       songs: [
         {
           name: this.songName,
           artist: this.creator,
-          url: "https://vffn2ikpzn353wl3p7bgvv32w2m6oebwrjt6wqfurswv6tpffj5a.arweave.net/qUrdIU_Ld93Ze3_Catd6tpnnEDaKZ-tAtIytX03lKno",
+          url: this.audioSrc,
         },
       ],
     });
+  }
+  volumeColor() {
+    let volumeRange = document.getElementById("volume-slider") as HTMLInputElement;
+    var value = (parseFloat(volumeRange.value) / 100) * 100;
+    volumeRange.style.background =
+      "linear-gradient(to right, #3A3A3A 0%, #3A3A3A " + value + "%, #EEEEEE " + value + "%, #EEEEEE 100%)";
   }
 }

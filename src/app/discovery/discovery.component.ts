4d8fd479d7209f8f13fff024c1cffa62e023a3ed
rajnikant <rajnikant.gupta@appinventiv.com>
2021-12-18 12:02:04 +0530

    ui fixes

---
+++ src/app/discovery/discovery.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+import { Router } from "@angular/router";
+
+@Component({
+  selector: 'app-discovery',
+  templateUrl: './discovery.component.html',
+  styleUrls: ['./discovery.component.scss']
+})
+export class DiscoveryComponent implements OnInit {
+
+  constructor(
+    //private route: DiscoveryRoute,
+    private router: Router
+  ) { }
+
+  ngOnInit(): void {
+  }
+
+}

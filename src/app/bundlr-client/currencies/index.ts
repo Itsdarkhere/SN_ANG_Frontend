618a984e2103644d6a9229e88fa3c1a4f990dd6f
Valtteri Juvonen <valtteri.juvone@hotmail.com>
2021-12-21 11:15:41 +0100

    Revert "remove local bundlr-client, use npm"
    This reverts commit bd1d948f02fb160e6e9929a620e4322830da9fca.

---
+++ src/app/bundlr-client/currencies/index.ts
@@ -0,0 +1,164 @@
+import redstone from "redstone-api";
+import BigNumber from "bignumber.js";
+import base64url from "base64url";
+import Arweave from "arweave";
+import { FileDataItem } from "arbundles/file";
+// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+// @ts-ignore
+
+import { keys } from "../bundlr";
+import {
+  maticCreateTx,
+  maticGetFee,
+  maticGetHeight,
+  maticGetPublicKey,
+  maticGetSigner,
+  maticGetTx,
+  maticOwnerToAddress,
+  maticSendTx,
+  maticSign,
+  maticVerify,
+} from "./matic";
+import { Signer } from "arbundles/src/signing";
+import {
+  solanaCreateTx,
+  solanaGetCurrentHeight,
+  solanaGetFee,
+  solanaGetPublicKey,
+  solanaGetSigner,
+  solanaGetTx,
+  solanaOwnerToAddress,
+  solanaSendTx,
+  solanaSign,
+  solanaVerify,
+} from "./solana";
+import {
+  arweaveCreateTx,
+  arweaveGetCurrentHeight,
+  arweaveGetFee,
+  arweaveGetId,
+  arweaveGetPublicKey,
+  arweaveGetSigner,
+  arweaveGetTx,
+  arweaveOwnerToAddress,
+  arweaveSendTx,
+  arweaveSign,
+  arweaveVerify,
+} from "./arweave";
+
+export interface Tx {
+  from: string;
+  to: string;
+  amount: BigNumber;
+  blockHeight?: BigNumber;
+  pending: boolean;
+  confirmed: boolean;
+}
+
+export interface CreateTxData {
+  amount: BigNumber | number;
+  to: string;
+  fee?: string;
+}
+
+export interface Currency {
+  base: [string, number];
+  account: { key: any; address: string };
+  provider?: string;
+
+  getTx(txId: string): Promise<Tx>;
+
+  ownerToAddress(owner: any): string;
+
+  getId(item: FileDataItem): Promise<string>;
+
+  price(): Promise<number>;
+
+  sign(data: Uint8Array): Promise<Uint8Array>;
+
+  getSigner(): Signer;
+
+  verify(pub: any, data: Uint8Array, signature: Uint8Array): Promise<boolean>;
+
+  getCurrentHeight(): Promise<BigNumber>;
+
+  getFee(amount: BigNumber | number, to?: string): Promise<BigNumber>;
+
+  sendTx(data: any): Promise<any>; //TODO: make signature(s) more specific
+
+  createTx(amount: BigNumber | number, to: string, fee?: string): Promise<{ txId: string; tx: any }>;
+
+  getPublicKey(): string | Buffer;
+}
+
+interface CurrencyConfig {
+  [key: string]: Currency;
+}
+
+export const currencies: CurrencyConfig = {
+  arweave: keys.arweave
+    ? {
+        base: ["winston", 1e12],
+        account: { key: keys.arweave.key, address: keys.arweave.address },
+        provider: "arweave.net",
+        getTx: arweaveGetTx,
+        ownerToAddress: arweaveOwnerToAddress,
+        getId: arweaveGetId,
+        price: () => getRedstonePrice("AR"),
+        sign: arweaveSign,
+        getSigner: arweaveGetSigner,
+        verify: arweaveVerify,
+        getCurrentHeight: arweaveGetCurrentHeight,
+        getFee: arweaveGetFee,
+        sendTx: arweaveSendTx,
+        createTx: arweaveCreateTx,
+        getPublicKey: arweaveGetPublicKey,
+      }
+    : undefined,
+  matic: keys.matic
+    ? {
+        base: ["wei", 1e18],
+        account: { key: keys.matic.key, address: keys.matic.address },
+        provider: "https://polygon-rpc.com",
+        getTx: maticGetTx,
+        getId: async (item) => {
+          return base64url.encode(Buffer.from(await Arweave.crypto.hash(await item.rawSignature())));
+        },
+        ownerToAddress: maticOwnerToAddress,
+        price: () => getRedstonePrice("MATIC"),
+        sign: maticSign,
+        getSigner: maticGetSigner,
+        verify: maticVerify,
+        getCurrentHeight: maticGetHeight,
+        getFee: maticGetFee,
+        sendTx: maticSendTx,
+        createTx: maticCreateTx,
+        getPublicKey: maticGetPublicKey,
+      }
+    : undefined,
+  solana: keys.solana
+    ? {
+        base: ["lamports", 1_000_000_000], // 1e9
+        account: { key: keys.solana.key, address: keys.solana.address },
+        provider: "mainnet-beta",
+        getTx: solanaGetTx,
+        getId: async (item) => {
+          return base64url.encode(Buffer.from(await Arweave.crypto.hash(await item.rawSignature())));
+        },
+        ownerToAddress: solanaOwnerToAddress,
+        price: () => getRedstonePrice("SOL"),
+        sign: solanaSign,
+        getSigner: solanaGetSigner,
+        verify: solanaVerify,
+        getCurrentHeight: solanaGetCurrentHeight,
+        getFee: solanaGetFee,
+        sendTx: solanaSendTx,
+        createTx: solanaCreateTx,
+        getPublicKey: solanaGetPublicKey,
+      }
+    : undefined,
+};
+
+export async function getRedstonePrice(currency: string): Promise<number> {
+  return (await redstone.getPrice(currency)).value;
+}
---
+++ src/app/bundlr-client/fund.ts
@@ -0,0 +1,29 @@
+import Utils from "./utils";
+export default class Fund {
+  private utils: Utils;
+
+  constructor(utils: Utils) {
+    this.utils = utils;
+  }
+
+  public async fund(amount: number, multiplier = 1.0): Promise<any> {
+    if (!Number.isInteger(amount)) {
+      throw new Error("must use an integer for funding amount");
+    }
+    const c = this.utils.currencyConfig;
+    const to = await this.utils.getBundlerAddress(this.utils.currency);
+    let baseFee;
+    if (c.base[0] === "winston") {
+      baseFee = await c.getFee(0, to);
+    } else {
+      baseFee = await c.getFee(amount, to);
+    }
+    const fee = baseFee.multipliedBy(multiplier).toFixed(0).toString();
+    const tx = await c.createTx(amount, to, fee.toString());
+    const nres = await c.sendTx(tx.tx);
+    Utils.checkAndThrow(nres, `Sending transaction to the ${this.utils.currency} network`);
+    const bres = await this.utils.api.post(`/account/balance/${this.utils.currency}`, { tx_id: tx.txId });
+    Utils.checkAndThrow(bres, "Posting transaction information to the bundler");
+    return { reward: fee, target: to, quantity: amount, id: tx.txId };
+  }
+}

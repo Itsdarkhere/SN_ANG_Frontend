40cdcbc9a31eb6af12bfe8a27b4876256a7223bf
a.gebril <a.gebril@metro-markets.de>
2021-12-22 19:21:33 +0100

    Use feed post icon row in nft post component with configurable paths

--- src/app/nft-post-page/nft-post/nft-post.component.ts
+++ src/app/nft-post-page/nft-post/nft-post.component.ts
@@ -26,7 +26,9 @@ import { SharedDialogs } from "src/lib/shared-dialogs";
 import { CommentModalComponent } from "src/app/comment-modal/comment-modal.component";
 import { GoogleAnalyticsService } from "src/app/google-analytics.service";
 import { FeedPostImageModalComponent } from 'src/app/feed/feed-post-image-modal/feed-post-image-modal.component';
-import { Meta, MetaDefinition } from '@angular/platform-browser';
+import { Meta } from '@angular/platform-browser';
+import { FIRST_ICON_PATH, SECOND_ICON_PATH, THIRD_ICON_PATH,
+  FOURTH_ICON_PATH, FIFTH_ICON_PATH } from 'src/app/feed/shared/constants/defines';
 
 @Component({
   selector: "nft-post",
@@ -55,9 +57,20 @@ export class NftPostComponent implements OnInit {
   owners: NFTEntryResponse[];
   hightestBidOwner: any = {};
   NftPostComponent = NftPostComponent;
-
   activeTab = NftPostComponent.THREAD;
   properties: any;
+  showIconRow = true;
+  firstIconPath = FIRST_ICON_PATH;
+  secondIconPath = SECOND_ICON_PATH;
+  thirdIconPath = THIRD_ICON_PATH
+  fourthIconPath = FOURTH_ICON_PATH;
+  fifthIconPath = FIFTH_ICON_PATH;
+
+  canReplaceExistingIcons = true;
+  _post: any;
+  postContent: any;
+  reposterProfile: any;
+  quotedContent: any;
 
   static ALL_BIDS = "All Bids";
   static MY_BIDS = "My Bids";
@@ -179,7 +192,8 @@ export class NftPostComponent implements OnInit {
         }
         // Set current post
         this.nftPost = res.PostFound;
-        console.log(this.nftPost)
+        this.configurePostType(this.nftPost);
+        console.log(this.nftPost, 'coming from nft post component');
         this.titleService.setTitle(this.nftPost.ProfileEntryResponse.Username + ` on ${environment.node.name}`);
         this.refreshBidData();
         this.configureMetaTags();
@@ -203,7 +217,7 @@ export class NftPostComponent implements OnInit {
       .subscribe(
         (res) => {
           this.nftBidData = res;
-          console.log(NftPostComponent)
+          console.log(this.nftBidData,'here is the key');
           if (!this.nftBidData.BidEntryResponses) {
             this.nftBidData.BidEntryResponses = [];
           }
@@ -622,9 +636,35 @@ export class NftPostComponent implements OnInit {
   }
 
   configureMetaTags(): void {
-  const imageUrl = this.mapImageURLs(this.nftPost?.ImageURLs[0]);
-  const nftDescription = this.nftPost?.PostExtraData?.name;
-  this.metaService.updateTag( { property:'og:url', content:`${imageUrl}`}, "property='og:url'");
-  this.metaService.updateTag( { property:'og:title', content:`${nftDescription}`}, "property='og:title'");
-}
+    const imageUrl = this.mapImageURLs(this.nftPost?.ImageURLs[0]);
+    const nftDescription = this.nftPost?.PostExtraData?.name;
+    this.metaService.updateTag({ property: 'og:url', content: `${imageUrl}` }, "property='og:url'");
+    this.metaService.updateTag({ property: 'og:title', content: `${nftDescription}` }, "property='og:title'");
+  }
+
+  isRepost(post: any): boolean {
+    return post.Body === "" && (!post.ImageURLs || post.ImageURLs?.length === 0) && post.RepostedPostEntryResponse;
+  }
+  isQuotedClout(post: any): boolean {
+    return (post.Body !== "" || post.ImageURLs?.length > 0) && post.RepostedPostEntryResponse;
+  }
+  isRegularPost(post: any): boolean {
+    return !this.isRepost(post) && !this.isQuotedClout(post);
+  }
+
+  configurePostType(post: any): void {
+    if (this.isRepost(post)) {
+      this.postContent = post.RepostedPostEntryResponse;
+      this.reposterProfile = post.ProfileEntryResponse;
+      if (this.isQuotedClout(post?.RepostedPostEntryResponse)) {
+        this.quotedContent = this.postContent.RepostedPostEntryResponse;
+      }
+    } else if (this.isQuotedClout(post)) {
+      this.postContent = post;
+      this.quotedContent = post.RepostedPostEntryResponse;
+    } else {
+      this.postContent = post;
+    }
+  }
+
 }

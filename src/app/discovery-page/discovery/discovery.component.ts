fe493196562943da11aeaa76af5e1a26eb2bc451
Valtteri Juvonen <valtteri.juvone@hotmail.com>
2022-01-02 18:40:10 +0100

    add discovery main card to discovery page

--- src/app/discovery-page/discovery/discovery.component.ts
+++ src/app/discovery-page/discovery/discovery.component.ts
@@ -1,19 +1,16 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
 import { Router } from "@angular/router";
 
 @Component({
-  selector: 'app-discovery',
-  templateUrl: './discovery.component.html',
-  styleUrls: ['./discovery.component.scss']
+  selector: "app-discovery",
+  templateUrl: "./discovery.component.html",
+  styleUrls: ["./discovery.component.scss"],
 })
 export class DiscoveryComponent implements OnInit {
-
   constructor(
     //private route: DiscoveryRoute,
     private router: Router
-  ) { }
+  ) {}
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }

c7616a4ca4678909f306b78f8357de4b486e1f85
Valtteri Juvonen <valtteri.juvone@hotmail.com>
2022-01-21 13:03:29 +0200

    remove old page-small naw and refactor to small-nav-new

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -173,7 +173,6 @@ import { TransferModalComponent } from "./transfer-modal/transfer-modal.componen
 import { AngularFireModule } from "@angular/fire";
 import { AngularFireStorageModule } from "@angular/fire/storage";
 import { environment } from "src/environments/environment.prod";
-import { PageSmallNavComponent } from "./page-small-nav/page-small-nav.component";
 import { SmallLeftBarComponent } from "./small-left-bar/small-left-bar.component";
 import { MarketplaceLeftBarComponent } from "./marketplace-left-bar/marketplace-left-bar.component";
 import { MarketplaceLeftBarMobileComponent } from "./marketplace-left-bar-mobile/marketplace-left-bar-mobile.component";
@@ -345,7 +344,6 @@ const coderTheme: Theme = { key: "light", name: "Coder Theme" };
     TransferModalComponent,
     AdminNodeFeesComponent,
     AdminNodeAddFeesComponent,
-    PageSmallNavComponent,
     SmallLeftBarComponent,
     MarketplaceLeftBarComponent,
     MarketplaceLeftBarMobileComponent,

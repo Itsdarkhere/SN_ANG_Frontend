92bfac5c7e75e880aeec7574c4d85864f396f067
Chiara Wilden <77884268+chiarawilden@users.noreply.github.com>
2022-02-07 20:18:25 -0500

    Add test input to collection-selections component

--- src/app/collections/create-collection/collection-selections/collection-selections.component.ts
+++ src/app/collections/create-collection/collection-selections/collection-selections.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-collection-selections',
@@ -7,7 +8,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CollectionSelectionsComponent implements OnInit {
 
-  constructor() { }
+  @Input() createCollectionForm: FormGroup;
 
   ngOnInit(): void {
   }

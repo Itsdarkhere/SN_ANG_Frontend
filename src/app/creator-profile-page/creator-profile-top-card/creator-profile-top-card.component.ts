656468cdee8f4fe88eca689899e5ced59d81148c
Felix G <felix@Felixs-MacBook-Pro.local>
2022-02-09 00:19:55 +0100

    responsive box

--- src/app/creator-profile-page/creator-profile-top-card/creator-profile-top-card.component.ts
+++ src/app/creator-profile-page/creator-profile-top-card/creator-profile-top-card.component.ts
@@ -237,7 +237,7 @@ export class CreatorProfileTopCardComponent implements OnInit, OnDestroy {
   }
   copyWalletAddress() {
     navigator.clipboard.writeText(this.profile.PublicKeyBase58Check);
-    document.getElementById("copyWallet").className += " wallet-button-click";
+    // document.getElementById("copyWallet").className += " wallet-button-click";
     
 
   }

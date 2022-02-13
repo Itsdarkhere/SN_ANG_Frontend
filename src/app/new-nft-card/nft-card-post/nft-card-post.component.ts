4b4aa32abc06c2cd6a1b0c7ccadbfc1ba795fc8a
Valtteri Juvonen <valtteri.juvone@hotmail.com>
2022-02-13 16:00:06 +0200

    add new files for nft card rewrite

---
+++ src/app/new-nft-card/nft-card-post/nft-card-post.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-nft-card-post',
+  templateUrl: './nft-card-post.component.html',
+  styleUrls: ['./nft-card-post.component.scss']
+})
+export class NftCardPostComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
---
+++ src/app/new-nft-card/nft-card-repost/nft-card-repost.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-nft-card-repost',
+  templateUrl: './nft-card-repost.component.html',
+  styleUrls: ['./nft-card-repost.component.scss']
+})
+export class NftCardRepostComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}

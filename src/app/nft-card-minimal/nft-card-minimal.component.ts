adafa460120d9980cdb1fe4d23d2b28e0721d73e
Chiara Wilden <77884268+chiarawilden@users.noreply.github.com>
2022-02-13 23:24:41 -0500

    File cleanup

--- src/app/nft-card-minimal/nft-card-minimal.component.ts
+++ src/app/nft-card-minimal/nft-card-minimal.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { BackendApiService, NFTEntryResponse, PostEntryResponse } from '../backend-api.service';
+import { GlobalVarsService } from "../global-vars.service";
 
 @Component({
   selector: 'app-nft-card-minimal',
@@ -7,9 +9,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NftCardMinimalComponent implements OnInit {
 
-  constructor() { }
+  @Input() nftData: PostEntryResponse;
+
+  constructor(
+    public globalVars: GlobalVarsService, 
+    private backendApi: BackendApiService
+  ) { }
+
+  nftEntryResponse: NFTEntryResponse[];
 
   ngOnInit(): void {
   }
-
 }

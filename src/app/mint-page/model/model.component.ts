25543ddeab77e476205bf5840806ec309de23ca1
Valtteri Juvonen <valtteri.juvone@hotmail.com>
2022-03-05 18:27:26 +0200

    remove borders from nft profile

--- src/app/mint-page/model/model.component.ts
+++ src/app/mint-page/model/model.component.ts
@@ -1,23 +1,21 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from "@angular/core";
 import "@google/model-viewer/dist/model-viewer";
 
 @Component({
-  selector: 'app-model',
-  templateUrl: './model.component.html',
-  styleUrls: ['./model.component.scss']
+  selector: "app-model",
+  templateUrl: "./model.component.html",
+  styleUrls: ["./model.component.scss"],
 })
 export class ModelComponent implements OnInit {
-
   @Input() postModelArweaveSrc: string;
 
-  modelSrc: string = "https://arweave.net/TE9t7DcyPA6bPU_lNx8tBzHzw7w5pt4-XaaFaXYemFc";
+  // modelSrc: string = "https://arweave.net/TE9t7DcyPA6bPU_lNx8tBzHzw7w5pt4-XaaFaXYemFc";
 
   // https://ipfs.io/ipfs/QmVLqaw72hFu2D4wii738ep4kXwbKZzQb4gutuJCUDMJxf/nft.glb
-  
-  constructor() { }
+
+  constructor() {}
 
   ngOnInit(): void {
     console.log(this.postModelArweaveSrc);
   }
-
 }

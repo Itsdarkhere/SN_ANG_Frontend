11efa113d415b1a092dd0a7dfbbed229551bf99b
Valtteri Juvonen <valtteri.juvone@hotmail.com>
2022-03-08 14:07:39 +0200

    ios scroll lock

--- src/app/global-vars.service.ts
+++ src/app/global-vars.service.ts
@@ -342,28 +342,25 @@ export class GlobalVarsService {
     // console.log(` ---------------- creator status is ${this.isCreator}`);
     // console.log(` ---------------- collector status is ${this.isCollector}`);
   }
-
+  scrollPosition: number;
+  body = document.querySelector("body");
   closeLeftBarMobile() {
     // Disable scroll
-    let scrollPosition: number;
-    let body = document.querySelector("body");
-    body.style.removeProperty("overflow");
-    body.style.removeProperty("position");
-    body.style.removeProperty("top");
-    body.style.removeProperty("width");
-    window.scrollTo(0, scrollPosition);
+    this.body.style.removeProperty("overflow");
+    this.body.style.removeProperty("position");
+    this.body.style.removeProperty("top");
+    this.body.style.removeProperty("width");
+    window.scrollTo(0, this.scrollPosition);
     // Close
     this.isLeftBarMobileOpen = false;
   }
   openLeftBarMobile() {
-    // Disable scroll
-    let scrollPosition: number;
-    let body = document.querySelector("body");
-    scrollPosition = window.pageYOffset;
-    body.style.overflow = "hidden";
-    body.style.position = "fixed";
-    body.style.top = `-${scrollPosition}px`;
-    body.style.width = "100%";
+    // Get scroll position
+    this.scrollPosition = window.pageYOffset;
+    this.body.style.overflow = "hidden";
+    this.body.style.position = "fixed";
+    this.body.style.top = `-${this.scrollPosition}px`;
+    this.body.style.width = "100%";
     // Close
     this.isLeftBarMobileOpen = true;
   }

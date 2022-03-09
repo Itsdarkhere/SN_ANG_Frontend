242d80adf70cf051411921248f3d8961a1ad6d4b
Kyle Reynolds <kyle.reynolds9146@gmail.com>
2022-03-09 12:04:41 -0500

    added variables to environment.prod

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
-  production: false,
+  production: true,
   uploadImageHostname: "node.deso.org",
   verificationEndpointHostname: "https://node.deso.org",
   uploadVideoHostname: "node.deso.org",
@@ -27,5 +27,15 @@ export const environment = {
     name: "Supernovas",
     url: "https://deso.org",
     logoAssetDir: "/assets/deso/",
+  },
+  imx: {
+    ROPSTEN_LINK_URL: "https://link.ropsten.x.immutable.com",
+    ROPSTEN_ENV_URL: "https://api.ropsten.x.immutable.com/v1",
+    ALCHEMY_API_KEY: "3DzfK-p8b3h4OBuZoAlwXEHf50apfpkG",
+    MINTER_PK: "707ea93fae5d6d311d09f2619bf49c9851210d0a3ec20d6798eb77da1beb1b47",
+    ROPSTEN_STARK_CONTRACT_ADDRESS: "0x4527be8f31e2ebfbef4fcaddb5a17447b27d2aef",
+    ROPSTEN_REGISTRATION_ADDRESS: "0x6C21EC8DE44AE44D0992ec3e2d9f1aBb6207D864",
+    TOKEN_ADDRESS: "0x657e183Fd98Ab568a4a3B740A6FcC8a972D6A5b3",
+    ROYALTY_ADDRESS: "0x8E923EE9057Fb6468bD62922a09cED67F1603c62",
   },
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,7 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: true,
+  production: false,
   uploadImageHostname: "node.deso.org",
   verificationEndpointHostname: "https://node.deso.org",
   uploadVideoHostname: "node.deso.org",

8a1a245f250bdb4c0ce517edf9ddae5eaecfadf8
Felix G <felix@Felixs-MacBook-Pro.local>
2022-03-11 22:07:09 +0100

    main video

--- src/app/landing-page-creators/landing-page-creators.component.ts
+++ src/app/landing-page-creators/landing-page-creators.component.ts
@@ -3,8 +3,8 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-landing-page-creators',
   templateUrl: './landing-page-creators.component.html',
-  styleUrls: ["./landing-page.component.scss"],})
-  
+  styleUrls: ['../landing-page/landing-page.component.scss']
+})
 export class LandingPageCreatorsComponent implements OnInit {
 
   constructor() { }
--- src/app/landing-page-dao/landing-page-dao.component.ts
+++ src/app/landing-page-dao/landing-page-dao.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-landing-page-dao',
   templateUrl: './landing-page-dao.component.html',
-  styleUrls: ['./landing-page.component.scss']
+  styleUrls: ['../landing-page/landing-page.component.scss']
 })
 export class LandingPageDaoComponent implements OnInit {
 

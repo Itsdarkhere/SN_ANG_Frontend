0ca47ae3a9505e09382e91893684774fb051af8a
Kyle Reynolds <kyle.reynolds9146@gmail.com>
2022-03-16 08:39:19 -0400

    enabled 3d minting

--- src/app/mint-page/mint-page.component.ts
+++ src/app/mint-page/mint-page.component.ts
@@ -169,6 +169,11 @@ export class MintPageComponent implements OnInit {
     } else {
       this.desoBlockchain = false;
     }
+
+    console.log(this.imageType);
+    console.log(this.videoType);
+    console.log(this.audioType);
+    console.log(this.modelType);
   }
   connectEthWallet() {
     this.openGeneralSuccessModal();

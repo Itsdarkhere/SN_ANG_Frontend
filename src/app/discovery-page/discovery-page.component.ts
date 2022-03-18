98537757baaa854259b55d7d1688b00e3cb1f002
Valtteri Juvonen <valtteri.juvone@hotmail.com>
2022-03-18 18:16:36 +0200

    discovery fixes

--- src/app/discovery-page/discovery-page.component.ts
+++ src/app/discovery-page/discovery-page.component.ts
@@ -13,71 +13,46 @@ import { Router } from "@angular/router";
 })
 // GlobalVars are used to disable loading if user allready has stuff loaded
 export class DiscoveryPageComponent implements OnInit {
-  @ViewChild("sliderRef1") sliderRef1: ElementRef<HTMLElement>
   @ViewChild("sliderRef2") sliderRef2: ElementRef<HTMLElement>
-  @ViewChild("sliderRef3") sliderRef3: ElementRef<HTMLElement>
-  @ViewChild("sliderRef4") sliderRef4: ElementRef<HTMLElement>
   @ViewChild("sliderRef5") sliderRef5: ElementRef<HTMLElement>
 
-  slider1: KeenSliderInstance = null
+
   slider2: KeenSliderInstance = null
-  slider3: KeenSliderInstance = null
-  slider4: KeenSliderInstance = null
   slider5: KeenSliderInstance = null
 
   discoveryDataToShow: { NFTEntryResponses: NFTEntryResponse[]; PostEntryResponse: PostEntryResponse }[];
   discoveryDataToShow2: { NFTEntryResponses: NFTEntryResponse[]; PostEntryResponse: PostEntryResponse }[];
+  discoveryDataToShow3: { NFTEntryResponses: NFTEntryResponse[]; PostEntryResponse: PostEntryResponse }[];
+  discoveryDataToShow4: { NFTEntryResponses: NFTEntryResponse[]; PostEntryResponse: PostEntryResponse }[];
+  discoveryDataToShow5: { NFTEntryResponses: NFTEntryResponse[]; PostEntryResponse: PostEntryResponse }[];
+
+  postsLoading = true;
+  postsLoading2 = true;
+  postsLoading3 = true;
+  postsLoading4 = true;
+  postsLoading5 = true;
+
+
   discoveryMainNftResponse: PostEntryResponse;
   discoveryUserArray: string[];
   discoveryExtraUserArray: string[];
 
   mobile = false;
-  postsLoading = false;
-  posts2Loading = false;
   usersLoading = false;
   fakeArray = [1, 2, 3, 4, 5, 6, 7, 8];
   constructor(private backendApi: BackendApiService, private mixPanel: MixpanelService, public globalVars: GlobalVarsService, public router: Router) {}
 
   ngOnInit(): void {
     this.setMobileBasedOnViewport();
-    // We could use this to not load again coming from nfts-page but It still seems to have some issues
-    // So for now we load everytime
-    /*if (
-      !this.globalVars.discoveryUserArray &&
-      !this.globalVars.discoveryDataToShow &&
-      !this.globalVars.discoveryDataToShow2
-    ) {
-      this.getCommunityFavourites();
-      this.getFreshDrops();
-      this._loadVerifiedUsers();
-    }*/
-    this.getCommunityFavourites();
-    this.getFreshDrops();
-    this._loadVerifiedUsers();
-
+    // Get only trending on init, others we get through the observeVisibility directive
+    // This is to reduce network load / postgres stress
+    this.getTrendingAuctions();
     this.mixPanel.track44("Discovery viewed");
   }
   setMobileBasedOnViewport() {
     this.mobile = this.globalVars.isMobile();
   }
   ngAfterViewInit() {
-    if (this.mobile) {
-      this.slider1 = new KeenSlider(this.sliderRef1.nativeElement, {
-        loop: false,
-        mode: "free-snap",
-        slides: { perView: "auto", spacing: 20  },
-      })
-      this.slider3 = new KeenSlider(this.sliderRef3.nativeElement, {
-        loop: false,
-        mode: "free-snap",
-        slides: { perView: "auto", spacing: 20  },
-      })
-      this.slider4 = new KeenSlider(this.sliderRef4.nativeElement, {
-        loop: true,
-        mode: "free-snap",
-        slides: { perView: "auto", spacing: 10  },
-      })
-    } 
     this.slider2 = new KeenSlider(this.sliderRef2.nativeElement, {
       loop: true,
       mode: "free-snap",
@@ -90,11 +65,6 @@ export class DiscoveryPageComponent implements OnInit {
     })
   }
   ngOnDestroy() {
-    if (this.mobile) {
-      if (this.slider1) this.slider1.destroy();
-      if (this.slider3) this.slider3.destroy();
-      if (this.slider4) this.slider4.destroy();
-    }
     if (this.slider2) this.slider2.destroy();
     if (this.slider5) this.slider5.destroy();
   }
@@ -103,6 +73,10 @@ export class DiscoveryPageComponent implements OnInit {
     this.setMobileBasedOnViewport();
   }
   getCommunityFavourites() {
+    // dont get if we have the data
+    if (this.discoveryDataToShow) {
+      return;
+    }
     this.postsLoading = true;
     this.backendApi
       .GetCommunityFavourite(
@@ -111,13 +85,57 @@ export class DiscoveryPageComponent implements OnInit {
         "BC1YLiiQ36NSLSK2bpLqi4PsP85mzBaKRTLxBAoTdNELohuRdrSMX9w",
       )
       .subscribe((res) => {
-        // For big image
-        this.discoveryMainNftResponse = res["PostEntryResponse"][0];
         this.discoveryDataToShow = res["PostEntryResponse"].slice(1, 9);
         setTimeout(() => {
           this.postsLoading = false;
         }, 300);
       });
+  }
+  getTrendingAuctions() {
+    // dont get if we have the data
+    if (this.discoveryDataToShow3) {
+      return;
+    }
+    this.postsLoading3 = true;
+    this.backendApi.GetTrendingAuctions(this.globalVars.localNode, this.globalVars?.loggedInUser?.PublicKeyBase58Check).subscribe((res) => {
+      this.discoveryMainNftResponse = res["PostEntryResponse"][0]
+      this.discoveryDataToShow3 = res["PostEntryResponse"];
+      setTimeout(() => {
+        this.postsLoading3 = false;
+      }, 350)
+    },(err) => {
+      console.log(err);
+    })
+  }
+  getRecentSales() {
+    // dont get if we have the data
+    if (this.discoveryDataToShow4) {
+      return;
+    }
+    this.postsLoading4 = true;
+    this.backendApi.GetRecentSales(this.globalVars.localNode, this.globalVars?.loggedInUser?.PublicKeyBase58Check).subscribe((res) => {
+      this.discoveryDataToShow4 = res["PostEntryResponse"];
+      setTimeout(() => {
+        this.postsLoading4 = false;
+      }, 350)
+    },(err) => {
+      console.log(err);
+    })
+  }
+  getSecondaryListings() {
+    // dont get if we have the data
+    if (this.discoveryDataToShow5) {
+      return;
+    }
+    this.postsLoading5 = true;
+    this.backendApi.GetSecondaryListings(this.globalVars.localNode, this.globalVars?.loggedInUser?.PublicKeyBase58Check).subscribe((res) => {
+      this.discoveryDataToShow5 = res["PostEntryResponse"];
+      setTimeout(() => {
+        this.postsLoading5 = false;
+      }, 350)
+    },(err) => {
+      console.log(err);
+    })
   }
   routeViewAll(category: string) {
     this.router.navigate([this.globalVars.RouteNames.NFT_PAGE], {
@@ -128,6 +146,10 @@ export class DiscoveryPageComponent implements OnInit {
     });
   }
   _loadVerifiedUsers() {
+    // Dont load if we have the content
+    if (this.discoveryUserArray) {
+      return;
+    }
     this.usersLoading = true;
     this.backendApi
       .AdminGetVerifiedUsers(this.globalVars.localNode, "BC1YLiiQ36NSLSK2bpLqi4PsP85mzBaKRTLxBAoTdNELohuRdrSMX9w")
@@ -153,7 +175,11 @@ export class DiscoveryPageComponent implements OnInit {
     document.getElementById(id).scrollIntoView({ behavior: "smooth", block: "start" });
   }
   getFreshDrops() {
-    this.posts2Loading = true;
+    // dont get if we have the data
+    if (this.discoveryDataToShow2) {
+      return;
+    }
+    this.postsLoading2 = true;
     this.backendApi
       .GetFreshDrops(
         this.globalVars.localNode,
@@ -163,7 +189,7 @@ export class DiscoveryPageComponent implements OnInit {
       .subscribe((res) => {
         this.discoveryDataToShow2 = res["PostEntryResponse"].slice(0, 8);
         setTimeout(() => {
-          this.posts2Loading = false;
+          this.postsLoading2 = false;
         }, 300);
       });
   }

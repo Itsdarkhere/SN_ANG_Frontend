1c0276d9c58e211939984144c83f47cd9752d5d8
Valtteri Juvonen <valtteri.juvone@hotmail.com>
2022-03-27 12:14:22 +0300

    another similar addition as prev

--- src/app/creator-profile-page/creator-profile-details/creator-profile-details.component.ts
+++ src/app/creator-profile-page/creator-profile-details/creator-profile-details.component.ts
@@ -229,6 +229,7 @@ export class CreatorProfileDetailsComponent implements OnInit {
     "BC1YLiuQcvSqFzRUaoSChucEXWSrj68Kmaepnbj7iQfo7eFzpBFVYJ2",
     "BC1YLiJziwbto5Loxdgq72wpJdAxqK5NU8ngoxGrVF14jt5h5k8uRTV",
     "BC1YLgXj87QsqYEQMQMgcMKDHqnxcsonMmtoA6vJMg5N9fd5j6sHrez",
+    "BC1YLfibiKTP1kgacxP6ks7uZ1Ygm9UjgVxH3BoK79S1kfqn1Hjog6k",
   ];
   checkIfAddCollections() {
     if (this.profile?.PublicKeyBase58Check == this.globalVars?.loggedInUser?.PublicKeyBase58Check) {
